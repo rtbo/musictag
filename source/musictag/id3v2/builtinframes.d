b103c7abc1ee8cb35a61bbc8801a01f7e08c60bf
Remi Thebault <remi.thebault@gmail.com>
2016-11-09 02:11:18 +0100

    TextInformationFrame

--- source/musictag/id3v2/builtinframes.d
+++ source/musictag/id3v2/builtinframes.d
@@ -21,9 +21,36 @@ private:
     void parse(const(ubyte)[] data)
     {
         _owner = decodeLatin1(data);
-        _data = data;
+        _data = data.dup;
     }
 
     string _owner;
     ubyte[] _data;
+}
+
+
+class TextInformationFrame : Frame
+{
+    this(const ref FrameHeader header, const(ubyte)[] data)
+    {
+        assert(header.id.length && header.id[0] == 'T' && header.id != "TXXX");
+        super(header);
+        parse(data);
+    }
+
+    @property string text() const { return _text; }
+
+private:
+
+    void parse(const(ubyte)[] data)
+    {
+        import std.exception : enforce;
+        enforce(data.length > 0);
+        auto encodingByte = data[0];
+        data = data[1 .. $];
+        _text = decodeString(data, encodingByte);
+        enforce(data.length == 0);
+    }
+
+    string _text;
 }
